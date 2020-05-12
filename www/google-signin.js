b2999be2139fdf2c220a4ea03b5c466612e2a0b0
Vivek Patel <vivek.patel371@gmail.com>
2020-05-12 23:23:31 +0400

    Credentials Removed

--- www/google-signin.js
+++ www/google-signin.js
@@ -3,10 +3,6 @@ cordova.addConstructor(function() {
 
     }
 
-    GoogleSignin.prototype.setServerClientId = function( serverClientId, successCallback, errorCallback ){
-        cordova.exec(successCallback, errorCallback, "GoogleSignin", "setServerClientId", [serverClientId]);
-    }
-
     GoogleSignin.prototype.login = function(options, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "GoogleSignin", "glogin", [options]);
     };
